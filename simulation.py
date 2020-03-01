2d5d2a2adf8ff1e15cbce745c7bfe42488d5956f
Kattenelvis <officialkattenelvis@gmail.com>
2020-03-01 19:23:11 +0100

    a

--- simulation.py
+++ simulation.py
@@ -1,5 +1,5 @@
 import xlsxwriter
-
+import math
 
 class gridTile():
     def __init__(self, x, y, z, temperature):
@@ -7,26 +7,40 @@ class gridTile():
         self.temperature = temperature #Average grid tile temperature
         self.neighbours = []
 
+
     def __repr__(self):
         return "grid tile with position x="+str(self.position[0])+" y="+str(self.position[1])+" and temperature="+str(self.temperature)+"C"
 
 
-class earthGrid():
+class PlanetaryGrid():
     def __init__(self, x, y, z):
         self.size = (x,y,z)
         self.tiles = []
         self.__generateGrid()
+
 
     def __repr__(self):
         return "Earth Grid with size x="+str(self.size[0])+" y="+str(self.size[1])
 
 
     def __generateGrid(self):
-        for x in range(self.size[0]):
+        for y in range(self.size[1]):
             self.tiles.append([])
-            for y in range(self.size[1]):
-                tile = gridTile(x,y,0,15+x+y)
-                self.tiles[x].append(tile)
+            for x in range(self.size[0]):
+                tile = gridTile(x,y,0,15.0)#math.cos(y)) Gona use sine waves to represent the polar regions vs the equator
+                self.tiles[y].append(tile)
+
+
+    def findNeighbours(self, x, y):
+        n = [-1, 0, 1]
+        neighbors = []
+        for y in range(3):
+            for x in range(3):
+                try:
+                    neighbours.append(self.tiles[x+n[j]][y+n[i]])
+                except:
+                    pass
+        return neighbors
 
 
 def printGrid(grid, book):
@@ -38,6 +52,7 @@ def printGrid(grid, book):
     return sheet
 
 book = xlsxwriter.Workbook("out.xlsx")
-e = earthGrid(8,8,4)
+e = PlanetaryGrid(8,8,4)
 sheet = printGrid(e, book)
 book.close()
+print(e.findNeighbours(3,3))
