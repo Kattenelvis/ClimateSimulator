ef0c4232fcf7f27e55cc3555b22980911da3e6b6
Kattenelvis <officialkattenelvis@gmail.com>
2020-03-11 15:49:08 +0100

    lets pray

--- code/main.py
+++ code/main.py
@@ -1,6 +1,7 @@
 import xlsxwriter
 from simulation.PlanetaryGrid import PlanetaryGrid
 from excel import printGrid
+from shutil import move
 
 grid = PlanetaryGrid(x=25,y=25,z=4,tempAmplitude=22.0,baseTemp=3.0)
 book = xlsxwriter.Workbook("out.xlsx")
@@ -12,3 +13,8 @@ for _ in range(b):
     grid.tiles = grid.timeStep()
 
 book.close()
+
+from os import getcwd, mkdir, rmdir, removedirs
+
+
+removedirs(getcwd()+"/output")
